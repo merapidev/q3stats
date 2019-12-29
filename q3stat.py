6838af74204eeff6dcd428b1b61f478fefaf3a44
Michal Szczepanski <michal@merapidev.com>
2019-12-29 22:09:03 +0100

    Draft of parsing files with logs

--- q3stat.py
+++ q3stat.py
@@ -1,6 +1,33 @@
+import os
+import re
 
 def start():
     print("Start")
+
+    regex = re.compile('^[0-9]{4}.[0-9]{2}.[0-9]{2}_#[0-9]+')
+
+    with os.scandir('files/') as files:
+        for file_with_logs in files:
+            
+            if not regex.match(file_with_logs.name):
+                continue
+            
+            print(file_with_logs.name)
+
+            with open(file_with_logs) as file_pointer:
+                for line in file_pointer:
+                    
+                    if 'Com_TouchMemory' in line:
+                        print('NEW GAME==================================')
+                        continue
+
+                    if 'RE_Shutdown' in line:
+                        print('END GAME==================================')
+                        continue
+
+                    if '^7' in line:
+                        print(line)
+
 
 
 if __name__ == "__main__":
